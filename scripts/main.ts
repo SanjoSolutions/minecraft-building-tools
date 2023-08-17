375e1c4efef614a380fd83e1d8969d3200796a93
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-08-17 21:15:13 +0200

    Copying and pasting

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,9 +1,15 @@
 import { world, system, Block, Entity, BlockPermutation } from "@minecraft/server"
 
+interface Space {
+  dimensions: {x: number, y: number, z: number}
+  state: (BlockPermutation | null)[]
+}
+
 interface PlayerState {
   firstBlock: Block | null
   lastCalled: number | null
   blockPermutation: BlockPermutation | null
+  savedSpace: Space | null
 }
 
 const state: Map<Entity, PlayerState> = new Map()
@@ -18,7 +24,8 @@ world.beforeEvents.itemUseOn.subscribe(function (event) {
       playerState = {
         firstBlock: null,
         lastCalled: null,
-        blockPermutation: null
+        blockPermutation: null,
+        savedSpace: null
       }
       state.set(event.source, playerState)
     }
@@ -37,7 +44,8 @@ world.beforeEvents.itemUseOn.subscribe(function (event) {
         playerState = {
           firstBlock: null,
           lastCalled: null,
-          blockPermutation: null
+          blockPermutation: null,
+          savedSpace: null
         }
         state.set(event.source, playerState)
       }
@@ -74,7 +82,8 @@ world.beforeEvents.itemUseOn.subscribe(function (event) {
         playerState = {
           firstBlock: null,
           lastCalled: null,
-          blockPermutation: null
+          blockPermutation: null,
+          savedSpace: null
         }
         state.set(event.source, playerState)
       }
@@ -111,7 +120,8 @@ world.beforeEvents.itemUseOn.subscribe(function (event) {
         playerState = {
           firstBlock: null,
           lastCalled: null,
-          blockPermutation: null
+          blockPermutation: null,
+          savedSpace: null
         }
         state.set(event.source, playerState)
       }
@@ -128,6 +138,80 @@ world.beforeEvents.itemUseOn.subscribe(function (event) {
           remove(playerState!.firstBlock.location.x, playerState!.firstBlock.location.y, playerState!.firstBlock.location.z, secondBlock.location.x, secondBlock.location.y, secondBlock.location.z)
           
           playerState!.firstBlock = null
+        }
+      })
+
+      playerState.lastCalled = now
+    }
+  } else if (event.itemStack.typeId === 'sanjo:copy_tool') {
+    const now = Date.now()
+
+    event.cancel = true
+
+    let playerState: PlayerState | undefined = state.get(event.source)
+
+    if (!playerState || !playerState.lastCalled || now - playerState.lastCalled > 300) {
+      if (!playerState) {
+        playerState = {
+          firstBlock: null,
+          lastCalled: null,
+          blockPermutation: null,
+          savedSpace: null
+        }
+        state.set(event.source, playerState)
+      }
+
+      system.run(function () {
+        if (playerState!.firstBlock === null) {
+          world.sendMessage('setting first block')
+          playerState!.firstBlock = event.block
+        } else {
+          world.sendMessage('copying')
+      
+          const secondBlock = event.block
+      
+          copy(playerState!.firstBlock.location.x, playerState!.firstBlock.location.y, playerState!.firstBlock.location.z, secondBlock.location.x, secondBlock.location.y, secondBlock.location.z, playerState!)
+          
+          playerState!.firstBlock = null
+        }
+      })
+
+      playerState.lastCalled = now
+    }
+  } else if (event.itemStack.typeId === 'sanjo:paste_tool') {
+    const now = Date.now()
+
+    event.cancel = true
+
+    let playerState: PlayerState | undefined = state.get(event.source)
+
+    if (!playerState || !playerState.lastCalled || now - playerState.lastCalled > 300) {
+      if (!playerState) {
+        playerState = {
+          firstBlock: null,
+          lastCalled: null,
+          blockPermutation: null,
+          savedSpace: null
+        }
+        state.set(event.source, playerState)
+      }
+
+      system.run(function () {
+        if (playerState!.firstBlock === null) {
+          world.sendMessage('setting first block')
+          playerState!.firstBlock = event.block
+        } else {
+          if (playerState?.savedSpace) {
+            world.sendMessage('pasting')
+        
+            const secondBlock = event.block
+        
+            paste(playerState!.firstBlock.location.x, playerState!.firstBlock.location.y, playerState!.firstBlock.location.z, secondBlock.location.x, secondBlock.location.y, secondBlock.location.z, playerState.savedSpace)
+
+            playerState!.firstBlock = null
+          } else {
+            world.sendMessage('Please copy space first before pasting.')
+          }
         }
       })
 
@@ -172,6 +256,65 @@ function remove(fromX: number, fromY: number, fromZ: number, toX: number, toY: n
     for (let z = z1; z <= z2; z++) {
       for (let x = x1; x <= x2; x++) {
         overworld.getBlock({x, y, z})?.setPermutation(airPermutation)
+      }
+    }
+  }
+}
+
+function copy(fromX: number, fromY: number, fromZ: number, toX: number, toY: number, toZ: number, playerState: PlayerState) {
+  const dimensions = {
+    x: Math.abs(toX - fromX) + 1,
+    y: Math.abs(toY - fromY) + 1,
+    z: Math.abs(toZ - fromZ) + 1
+  }
+  const state = new Array(dimensions.x * dimensions.y * dimensions.z)
+  const savedSpace = {
+    dimensions,
+    state
+  }
+
+  const x1 = Math.min(fromX, toX)
+  const y1 = Math.min(fromY, toY)
+  const z1 = Math.min(fromZ, toZ)
+  const overworld = world.getDimension('overworld')
+  for (let y = 0; y < dimensions.y; y++) {
+    for (let z = 0; z < dimensions.z; z++) {
+      for (let x = 0; x < dimensions.x; x++) {
+        let savedPermutation
+        const block = overworld.getBlock({x: x1 + x, y: y1 + y, z: z1 + z})
+        if (block) {
+          const permutation = block.permutation
+          if (permutation.type.id === 'minecraft:air') {
+            savedPermutation = null
+          } else {
+            savedPermutation = permutation.clone()
+          }
+        } else {
+          savedPermutation = null
+        }
+
+         state[y * (dimensions.x * dimensions.z) + z * dimensions.x + x] = savedPermutation
+      }
+    }
+  }
+  playerState.savedSpace = savedSpace
+}
+
+function paste(fromX: number, fromY: number, fromZ: number, toX: number, toY: number, toZ: number, space: Space) {
+  const x1 = Math.min(fromX, toX)
+  const y1 = Math.min(fromY, toY)
+  const z1 = Math.min(fromZ, toZ)
+  const overworld = world.getDimension('overworld')
+  for (let y = 0; y < space.dimensions.y; y++) {
+    for (let z = 0; z < space.dimensions.z; z++) {
+      for (let x = 0; x < space.dimensions.x; x++) {
+        const permutation = space.state[y * (space.dimensions.x * space.dimensions.z) + z * space.dimensions.x + x]
+        if (permutation) {
+          const block = overworld.getBlock({x: x1 + x, y: y1 + y, z: z1 + z})
+          if (block) {
+            block.setPermutation(permutation)
+          }
+        }
       }
     }
   }
